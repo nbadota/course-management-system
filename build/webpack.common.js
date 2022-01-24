9ac296d247ae9d55f66740ec47d8b4273ed84596
nbadota <383973155@qq.com>
2022-01-24 13:14:07 +0800

    init project

---
+++ build/webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname,'..','src','index.tsx')
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'..','dist'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,'..','src','index.html')
+        })
+    ],
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    }
+}
---
+++ build/webpack.prod.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HappyPack = require('happypack');
+
+module.exports = merge(common,{
+    mode: 'production',
+    devtool: 'source-map',
+    module:{
+        rules:[
+            {
+                test: /\.(tsx|ts)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'happypack/loader?id=babel',
+                    options: {
+                        presets: ['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                type: 'asset',
+                generator: {
+                    filename: 'static/[hash][ext][query]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8192,// 8kb
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        // 抽离 css 文件
+        new MiniCssExtractPlugin({
+            filename: 'css/main.[contentHash:8].css'
+        }),
+        new HappyPack({
+            // 用唯一的标识符 id 来代表当前的 HappyPack 是用来处理一类特定的文件
+            id: 'babel',
+            // 如何处理 .js 文件，用法和 Loader 配置中一样
+            loaders: ['babel-loader?cacheDirectory']
+            // ... 其它配置项
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    }
+});
